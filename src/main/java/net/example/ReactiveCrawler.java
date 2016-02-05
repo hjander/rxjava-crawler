c2689b2422eb534d04787f67f2281684fc35f61d
hja <github@futureofwebtechnology.com>
2016-02-05 15:59:30 +0100

    use java.netURL instead of String + polish (constants + error handling)

--- src/main/java/net/example/ReactiveCrawler.java
+++ src/main/java/net/example/ReactiveCrawler.java
@@ -11,6 +11,7 @@ import com.mongodb.rx.client.MongoClients;
 import com.mongodb.rx.client.MongoCollection;
 import com.mongodb.rx.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.client.rx.RxClient;
 import org.glassfish.jersey.client.rx.rxjava.RxObservable;
@@ -19,10 +20,11 @@ import rx.Notification;
 import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
@@ -34,147 +36,170 @@ import static java.util.Arrays.asList;
 
 /**
  * @author Hendrik Jander
- *
  */
 public class ReactiveCrawler {
 
-	public static final String FIELD_NAME_URL = "url";
+    public static final String FIELD_NAME_URL = "url";
+    public static final String FIELD_NAME_STATUS = "status";
+
+    public static final String HOST = "192.168.99.100:27017";
+
+    enum CrawlStatus {
+        OK, PROGRESS, ERROR
+    }
 
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, MalformedURLException {
+
+        CountDownLatch cl = new CountDownLatch(1);
+
+        RxClient<RxObservableInvoker> client = RxObservable.newClient(Executors.newFixedThreadPool(32));
+        MongoCollection<Document> linkCollection = setupLinkCollection();
+
+        URL initialLink = new URL("http://www.zeit.de/index");
+
+        linkCollection.insertOne(new Document(FIELD_NAME_URL, initialLink.toExternalForm())).toBlocking().single();
 
 
-		CountDownLatch cl = new CountDownLatch(1);
+        Observable.interval(200, TimeUnit.MILLISECONDS).onBackpressureBlock()
+                .flatMap(tick -> links(linkCollection))
+                .flatMap(linkDoc -> httpGet(client, linkDoc)
+                        .map(HtmlLinkExtractor::parseLinks).map(ReactiveCrawler::linksAsDocuments)//.subscribeOn(Schedulers.computation())
+                        .flatMap(docs -> persist(linkCollection, linkDoc, docs)))//.subscribeOn(Schedulers.io())
+                .subscribe();
 
-		RxClient<RxObservableInvoker> client = RxObservable.newClient(Executors.newFixedThreadPool(32));
-		MongoCollection<Document> linkCollection = setupLinkCollection();
-
-		//String initialLink = "http://localhost:8888/testImgTagBasicAuth.html";
-		String initialLink = "http://www.faz.de";
-
-		linkCollection.insertOne(new Document("url", initialLink)).toBlocking().single();
-
-		
-		Observable.interval(200, TimeUnit.MILLISECONDS).onBackpressureBlock()
-			.flatMap(tick -> links(linkCollection))
-				.flatMap(linkDoc -> httpGet(client, linkDoc)
-						.map(HtmlLinkExtractor::parseLinks).map(ReactiveCrawler::linksAsDocuments).subscribeOn(Schedulers.computation())
-						.flatMap(docs -> persist(linkCollection, linkDoc, docs)))//.subscribeOn(Schedulers.io())
-				.subscribe();
-
-		cl.await();
-	}
+        cl.await();
+    }
 
 
-	static Observable<String> httpGet(RxClient<RxObservableInvoker> client, Document url) {
+    static Observable<String> httpGet(RxClient<RxObservableInvoker> client, Document toCrawl) {
 
-		System.out.printf("%s Getting document %s%n", Thread.currentThread(), url.getString("url"));
+        System.out.printf("%s Getting document %s%n", Thread.currentThread(), toCrawl.getString(FIELD_NAME_URL));
 
-		return client
-				.target(url.getString("url"))
-				.property(ClientProperties.FOLLOW_REDIRECTS, Boolean.TRUE)
-				.request().rx().get(Response.class)
-				.filter(response -> response.getHeaderString(HttpHeaders.CONTENT_TYPE).contains("html"))
-				.map(htmlResponse -> htmlResponse.readEntity(String.class))
-				.onErrorResumeNext(throwable -> {
-					System.out.printf("Error:"+throwable.getMessage());
-					return Observable.<String>empty();
-				});
+        return client
+                .target(toCrawl.getString(FIELD_NAME_URL))
+                .property(ClientProperties.FOLLOW_REDIRECTS, Boolean.TRUE)
+                .request().rx().get(Response.class)
 
-	}
+                .filter(response -> response.getHeaderString(HttpHeaders.CONTENT_TYPE).contains("html"))
+                .map(htmlResponse -> htmlResponse.readEntity(String.class))
+                .onErrorResumeNext(throwable -> {
+                    System.out.printf("ERROR:%s", throwable.getMessage());
+                    return Observable.empty();
+                });
 
-
-
-	static public Observable<Document> links(MongoCollection<Document> linkCollection) {
-
-		return linkCollection.findOneAndUpdate(Filters.ne("status", "DONE"),
-				new Document("$set", new Document("status", "DONE")),
-				new FindOneAndUpdateOptions().sort(Sorts.ascending(FIELD_NAME_URL)))
-					.doOnNext(document -> {
-						System.out.printf("%s Next Link: %s %n", Thread.currentThread(), document.getString(FIELD_NAME_URL));
-					});
-	}
-
-	
-	static public Observable<UpdateResult> persist(MongoCollection<Document> linkCollection, Document linkDoc, Set<Document> documents) {
-
-		Func1<Document, Observable<UpdateResult>> insert =
-				doc -> linkCollection.updateOne(
-						Filters.eq("url", doc.getString("url")),
-						new Document("$set", doc).append("$currentDate", new Document("lastVisit", new Document("$type", "timestamp") ) ),
-						new UpdateOptions().upsert(true));
-
-		return Observable.from(documents).flatMap(insert)
-				.onBackpressureDrop(updateResult1 -> System.out.printf("BACKPESSURE"))
-				.doOnNext(updateResult ->
-						System.out.printf("%s Persisting Document id: %s , inserted: %b %n",
-								Thread.currentThread(), updateResult.getUpsertedId(), updateResult.getMatchedCount() == 0))
-				.onErrorResumeNext(throwable -> {
-					System.out.printf("Error:" + throwable.getMessage());
-					return Observable.empty();
-				});
-
-	}
+    }
 
 
-	private static MongoCollection<Document> setupLinkCollection() {
+    static public Observable<Document> links(MongoCollection<Document> linkCollection) {
 
-		ClusterSettings clusterSettings = ClusterSettings.builder()
-				.hosts(asList(new ServerAddress("192.168.99.105:27017"))).build();
-		ConnectionPoolSettings connectionPoolSettings = ConnectionPoolSettings.builder().maxSize(100)
-				.maxWaitQueueSize(100000).build();
+        Bson documentNotInProgressFilter = Filters.ne(FIELD_NAME_STATUS, CrawlStatus.PROGRESS.name());
+        Document setDocumentInProgress = new Document("$set", new Document(FIELD_NAME_STATUS, CrawlStatus.PROGRESS.name()));
+        FindOneAndUpdateOptions foo = new FindOneAndUpdateOptions().sort(Sorts.ascending(FIELD_NAME_URL));
 
-		MongoClientSettings settings = MongoClientSettings.builder().clusterSettings(clusterSettings)
-				.connectionPoolSettings(connectionPoolSettings).build();
+        return linkCollection.findOneAndUpdate(documentNotInProgressFilter, setDocumentInProgress, foo)
+                .doOnNext(document -> {
+                    System.out.printf("%s Next Link: %s %n", Thread.currentThread(), document.getString(FIELD_NAME_URL));
+                })
+                .doOnError(throwable -> {
+                    System.out.printf("ERROR %s", throwable.getMessage());
+                });
 
-		MongoClient mongoClient = MongoClients.create(settings);
-		MongoDatabase database = mongoClient.getDatabase("crawler");
-		MongoCollection<Document> collection = database.getCollection("links");
-
-		cleanup: {
-			collection.deleteMany(new Document()).toBlocking().single();
-		}
-
-		collection.createIndex(new Document(FIELD_NAME_URL, 1), new IndexOptions().unique(true).background(true))
-				.toBlocking().single();
-
-		return collection;
-	}
+    }
 
 
-	static public Set<Document> linksAsDocuments(Set<HtmlLinkExtractor.HtmlLink> links) {
+    static public Observable<UpdateResult> persist(MongoCollection<Document> linkCollection, Document parentDoc, Set<Document> linkedDocuments) {
 
-		return links.stream()
-				.filter(link -> link.getLink().startsWith("http://"))
-				.collect(HashSet::new,
-						(set, link) -> set.add(new Document("url", link.getLink()).append("linktext", link.getLinkText())),
-						HashSet::addAll);
-	}
+        Func1<Document, Observable<UpdateResult>> upsertDoc =
+                doc -> linkCollection.updateOne(
+                            Filters.eq(FIELD_NAME_URL, doc.getString(FIELD_NAME_URL)),
+                            new Document("$set", doc)
+                                .append("$currentDate", new Document("gathered", new Document("$type", "timestamp")))
+                                .append("$addToSet", new Document("incoming", parentDoc.getString(FIELD_NAME_URL))),
+                            new UpdateOptions().upsert(true));
 
 
+        return Observable.from(linkedDocuments).flatMap(upsertDoc)
+                .doOnNext(updateResult ->
+                        System.out.printf("%s Persisting Document id: %s , inserted: %s %n ackknowledged: %s",
+                                Thread.currentThread(), updateResult.getUpsertedId(),
+                                updateResult.getMatchedCount() == 0, updateResult.wasAcknowledged()))
 
-	static <T> Action1<Notification<? super T>> debug(String description, String offset) {
+                .onErrorResumeNext(throwable -> {
+                    System.out.printf("Error: %s", throwable.getMessage());
+                    return Observable.empty();
+                })
+                .doOnError(throwable -> linkCollection.updateOne(
+                        new Document(FIELD_NAME_URL, parentDoc.getString(FIELD_NAME_URL)),
+                        new Document(FIELD_NAME_STATUS, CrawlStatus.ERROR)))
 
-		AtomicReference<String> nextOffset = new AtomicReference<>(">");
-		return (Notification<? super T> notification) -> {
+                .doOnCompleted(() -> linkCollection.updateOne(
+                                new Document(FIELD_NAME_URL, parentDoc.getString(FIELD_NAME_URL)),
+                                new Document(FIELD_NAME_STATUS, CrawlStatus.OK))
+                );
+    }
 
-			switch (notification.getKind()) {
-			case OnNext:
-				System.out.println(Thread.currentThread().getName() + "|" + description + ": " + offset
-						+ nextOffset.get());
-				break;
-			case OnError:
-				System.err.println(Thread.currentThread().getName() + "|" + description + ": " + offset
-						+ nextOffset.get() + " X " + notification.getThrowable());
-				break;
-			case OnCompleted:
-				System.out.println(
-						Thread.currentThread().getName() + "|" + description + ": " + offset + nextOffset.get() + "|");
-			default:
-				break;
-			}
-			nextOffset.getAndUpdate(p -> "-" + p);
-		};
-	}
+
+    private static MongoCollection<Document> setupLinkCollection() {
+
+        ClusterSettings clusterSettings = ClusterSettings.builder()
+                .hosts(asList(new ServerAddress(HOST))).build();
+
+        ConnectionPoolSettings connectionPoolSettings = ConnectionPoolSettings.builder().maxSize(100)
+                .maxWaitQueueSize(100000).build();
+
+        MongoClientSettings settings = MongoClientSettings.builder().clusterSettings(clusterSettings)
+                .connectionPoolSettings(connectionPoolSettings).build();
+
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("crawler");
+        MongoCollection<Document> collection = database.getCollection("links");
+
+        cleanup:
+        {
+            collection.deleteMany(new Document()).toBlocking().single();
+        }
+
+        collection.createIndex(new Document(FIELD_NAME_URL, 1), new IndexOptions().unique(true).background(true))
+                .toBlocking().single();
+
+        return collection;
+    }
+
+
+    static public Set<Document> linksAsDocuments(Set<HtmlLinkExtractor.HtmlLink> links) {
+
+        return links.stream()
+                .filter(link -> link.getLink().getProtocol().equals("http"))
+                .collect(HashSet::new,
+                        (set, link) -> set.add(new Document(FIELD_NAME_URL, link.getLink().toExternalForm()).append("linktext", link.getLinkText())),
+                        HashSet::addAll);
+
+    }
+
+
+    static <T> Action1<Notification<? super T>> debug(String description, String offset) {
+
+        AtomicReference<String> nextOffset = new AtomicReference<>(">");
+        return (Notification<? super T> notification) -> {
+
+            switch (notification.getKind()) {
+                case OnNext:
+                    System.out.println(Thread.currentThread().getName() + "|" + description + ": " + offset
+                            + nextOffset.get());
+                    break;
+                case OnError:
+                    System.err.println(Thread.currentThread().getName() + "|" + description + ": " + offset
+                            + nextOffset.get() + " X " + notification.getThrowable());
+                    break;
+                case OnCompleted:
+                    System.out.println(
+                            Thread.currentThread().getName() + "|" + description + ": " + offset + nextOffset.get() + "|");
+                default:
+                    break;
+            }
+            nextOffset.getAndUpdate(p -> "-" + p);
+        };
+    }
 
 }
